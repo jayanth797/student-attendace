aa99de28955c07b913cd19ec0992ad5f0ee230d6
jayanthp797 <jayanthpjayanthp055@gmail.com>
2026-04-30 14:39:05 +0530

    Prepare files for attendance feature

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.mark_attendance, name='mark_attendance'),
     path('add-student/', views.add_student, name='add_student'),
+    path('delete-student/<int:student_id>/', views.delete_student, name='delete_student'),
     path('report/', views.attendance_report, name='attendance_report'),
     path('export/', views.export_csv, name='export_csv'),
+    path('export/excel/', views.export_excel, name='export_excel'),
 ]
